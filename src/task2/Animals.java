5d1199acc22584f76efeecfd7c988529880bee7b
Катерина <kate2002.kk42@gmail.com>
2023-10-24 14:30:22 +0300

    finish

--- src/task2/Animals.java
+++ src/task2/Animals.java
@@ -1,2 +1,22 @@
-package task2;public enum Animals {
+package task2;
+
+public enum Animals {
+    DOG(5),
+    CAT(2),
+    MONKEY(1),
+    LION(7);
+
+    private int age;
+
+    Animals(int age) {
+        this.age = age;
+    }
+    public String toString (){
+        return name() + " (вік " + age + " років )";
+    }
+
+    public static void main(String[] args) {
+        Animals myPet = Animals.CAT;
+        System.out.println("Моя улюбена тварина " + myPet);
+    }
 }
--- src/task4/Vehicles.java
+++ src/task4/Vehicles.java
@@ -1,2 +1,28 @@
-package task4;public enum Vehicles {
+package task4;
+
+public enum Vehicles {
+    CAR(35000, "Black"),
+    BIKE(12000, "Gray");
+
+    private int cost;
+    private String color;
+
+    Vehicles(int cost, String color) {
+        this.cost = cost;
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public String toString(){
+        return name() + " Колір: " + color + " Вартість: " + cost;
+    }
+
+    public static void main(String[] args) {
+        Vehicles myCar = Vehicles.CAR;
+        System.out.println("Мій автомобіль: " + myCar);
+    }
 }
+
+
